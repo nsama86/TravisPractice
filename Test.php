2e5d07f150cb58fcf2b9da4e803000410cd12b06
nsama86 <ja.ne.hw.1986@gmail.com>
2017-04-09 12:07:55 -0700

    modified travis with php script in it and added a new php class file called Test

---
+++ Test.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: janeh
+ * Date: 4/9/2017
+ * Time: 12:02 PM
+ */
+class Test extends PHPUnit_Framework_TestCase
+{
+  
+  public function testOne(){
+    
+    $this->assertEquals(1+1,2);
+  }
+
+}
